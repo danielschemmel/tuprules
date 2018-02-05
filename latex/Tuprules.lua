b463be6ce71a32140f9e3130291f8231d6fe9bb4
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2018-02-05 02:33:48 +0100

    updated rules

--- latex/Tuprules.lua
+++ latex/Tuprules.lua
@@ -6,10 +6,18 @@ tup.creategitignore()
 
 function quote(str)
 	-- this quotes a string in a manner safe for shell (well, bash) usage via tup
-	-- basically, this adds single quotes while escaping previously existing ones and doubles up on percentage signs
+	-- basically, this adds single quotes while escaping previously existing ones and doubles percentage signs
 	-- TODO: find a way to deal with the $() and @() tup syntax
 	-- TODO: fix tup, so that it really does not do substitutions when using definerule
 	return "'"..(str:gsub("'", "'\\''"):gsub("%%", "%%%%")).."'"
+end
+
+function quote_output(str)
+	-- this quotes a string in a manner safe for shell (well, bash) usage via tup as an *output* parameter
+	-- basically, this quadruples percentage signs
+	-- TODO: fix tup, so that it really does not do substitutions when using definerule
+	result,_ = str:gsub("%%", "%%%%%%%%")
+	return result
 end
 
 function concat(options)
@@ -44,7 +52,20 @@ function latexmk(options)
 	assert(texfile ~= nil)
 	assert(output ~= nil)
 	local outdir = doc_outdir(output)
-	local command = "env SOURCE_DATE_EPOCH="..quote(tostring(source_epoch)).." USER=author"
+	local command = "^o compiling %f^ env SOURCE_DATE_EPOCH="..quote(tostring(source_epoch)).." USER=author"
+	local input
+	if options.prelude == nil then
+		input = { texfile }
+	else
+		assert(type(options.prelude) == "string")
+		local prelude = outdir.."/prelude.tex"
+		input = { texfile, prelude }
+		tup.definerule{
+			command = "^o Writing prelude %o^ printf "..quote("%s\n%s").." "..quote(options.prelude).." "..quote("\\input{"..texfile.."}").." > "..quote(prelude),
+			outputs = { prelude },
+		}
+		texfile = prelude
+	end
 	if options.latex == nil or options.latex == "xelatex" then
 		command = command.." latexmk -pdf -xelatex -output-directory="..quote(outdir).." -jobname=result "..quote(texfile)
 	elseif options.latex == "lualatex" then
@@ -81,16 +102,19 @@ function latexmk(options)
 		table.insert(result_bonus, file)
 	end
 	command += "&& rm -f "..foo
-	local input = {texfile}
 	if type(options.extra_inputs) == "string" then
 		table.insert(input, options.extra_inputs)
 	elseif type(options.extra_inputs) == "table" then
 		tup.append_table(input, options.extra_inputs)
 	end
+	if options.build_group ~= nil then
+		assert(type(options.build_group) == "string")
+    table.insert(result_bonus, options.build_group)
+	end
 	tup.definerule{
 		inputs = input,
 		command = tostring(command),
-		outputs = result_bonus
+		outputs = result_bonus,
 	}
 	local outputs = {output}
 	if options.group ~= nil then
@@ -100,7 +124,7 @@ function latexmk(options)
 	tup.definerule{
 		inputs = {result},
 		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
-		outputs = outputs
+		outputs = outputs,
 	}
 end
 
@@ -115,57 +139,7 @@ function pdfcrop(options)
 	tup.definerule{
 		inputs = {input},
 		command = "^o Cropping %f^ pdfcrop "..quote(input).." "..quote(result),
-		outputs = {result}
-	}
-	local outputs = {output}
-	if options.group ~= nil then
-		assert(type(options.group) == "string")
-    table.insert(outputs, options.group)
-	end
-	tup.definerule{
-		inputs = {result},
-		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
-		outputs = {output}
-	}
-end
-
-function pdfcompress(options)
-	local input = options[1]
-	local ext = tup.ext(input)
-	assert(ext == "pdf")
-	local output
-	if options[2] == nil then output = input:sub(0,-5).."_cropped.pdf" else output = options[2] end
-	local outdir = doc_outdir(output)
-	local result = outdir..'/'..output
-	tup.definerule{
-		inputs = {input},
-		command = "^o Compressing %f^ gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -dAutoRotatePages=/None -sOutputFile="..quote(result).." "..quote(input),
-		outputs = {result}
-	}
-	local outputs = {output}
-	if options.group ~= nil then
-		assert(type(options.group) == "string")
-    table.insert(outputs, options.group)
-	end
-	tup.definerule{
-		inputs = {result},
-		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
-		outputs = {output}
-	}
-end
-
-function pdfgreyscale(options)
-	local input = options[1]
-	local ext = tup.ext(input)
-	assert(ext == "pdf")
-	local output
-	if options[2] == nil then output = input:sub(0,-5).."_grey.pdf" else output = options[2] end
-	local outdir = doc_outdir(output)
-	local result = outdir..'/'..output
-	tup.definerule{
-		inputs = {input},
-		command = "^o Creating greyscale version of %f^ gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -dAutoRotatePages=/None -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -sOutputFile="..quote(result).." "..quote(input),
-		outputs = {result}
+		outputs = {result},
 	}
 	local outputs = {output}
 	if options.group ~= nil then
@@ -176,6 +150,66 @@ function pdfgreyscale(options)
 		inputs = {result},
 		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
 		outputs = {output},
+	}
+end
+
+function pdfcompress(options)
+	local input = options[1]
+	local ext = tup.ext(input)
+	assert(ext == "pdf")
+	local output
+	if options[2] == nil then output = input:sub(0,-5).."_compressed.pdf" else output = options[2] end
+	local outdir = doc_outdir(output)
+	local result = outdir.."/result.pdf"
+	local results = { quote_output(result) }
+	if options.build_group ~= nil then
+		assert(type(options.build_group) == "string")
+    table.insert(results, options.build_group)
+	end
+	tup.definerule{
+		inputs = {input},
+		command = "^o Compressing %f^ gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -dAutoRotatePages=/None -sOutputFile="..quote(result:gsub("%%", "%%%%")).." "..quote(input),
+		outputs = results,
+	}
+	local outputs = { quote_output(output) }
+	if options.group ~= nil then
+		assert(type(options.group) == "string")
+    table.insert(outputs, options.group)
+	end
+	tup.definerule{
+		inputs = {result},
+		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
+		outputs = outputs,
+	}
+end
+
+function pdfgreyscale(options)
+	local input = options[1]
+	local ext = tup.ext(input)
+	assert(ext == "pdf")
+	local output
+	if options[2] == nil then output = input:sub(0,-5).."_grey.pdf" else output = options[2] end
+	local outdir = doc_outdir(output)
+	local result = outdir.."/result.pdf"
+	local results = { quote_output(result) }
+	if options.build_group ~= nil then
+		assert(type(options.build_group) == "string")
+    table.insert(results, options.build_group)
+	end
+	tup.definerule{
+		inputs = {input},
+		command = "^o Creating greyscale version of %f^ gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -dAutoRotatePages=/None -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -sOutputFile="..quote(result:gsub("%%", "%%%%")).." "..quote(input),
+		outputs = results,
+	}
+	local outputs = { quote_output(output) }
+	if options.group ~= nil then
+		assert(type(options.group) == "string")
+    table.insert(outputs, options.group)
+	end
+	tup.definerule{
+		inputs = {result},
+		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
+		outputs = outputs,
 	}
 end
 
@@ -190,7 +224,7 @@ function pdf2jpg(options)
 	tup.definerule{
 		inputs = {input},
 		command = "^o Creating jpg version of %f^ convert -density 600 -colorspace rgb "..quote(input).." "..quote(result),
-		outputs = {result}
+		outputs = {result},
 	}
 	local outputs = {output}
 	if options.group ~= nil then
@@ -200,6 +234,6 @@ function pdf2jpg(options)
 	tup.definerule{
 		inputs = {result},
 		command = "^o Copying out %o^ cp "..quote(result).." "..quote(output),
-		outputs = {output}
+		outputs = {output},
 	}
 end
